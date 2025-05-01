d9e38f68e6becc25d1e813225231e50572f50d77
surya <surya@surya>
2025-05-01 11:05:54 +0530

    Testcase2 added

---
+++ OrangeHRM/src/testcases/testcase2.java
@@ -0,0 +1,11 @@
+package testcases;
+
+public class testcase2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+  String s ="testcase";
+  System.out.println(s);
+	}
+
+}
